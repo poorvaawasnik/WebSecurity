32560b650b38e7e5e6301aee345a90e0b6b7606a
Poorvaa Wasnik <poorvaawasnik@gmail.com>
2025-12-08 23:28:17 +0530

    message

---
+++ src/main/java/com/example/websecurity/config/LoginSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.websecurity.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+@Configuration
+public class LoginSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        UserDetails user1 = User.withUsername("amit")
+                .password("{noop}12345")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1);
+
+    }
+}
---
+++ src/main/java/com/example/websecurity/controller/MyController.java
@@ -0,0 +1,20 @@
+package com.example.websecurity.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+    @RequestMapping("/")
+    public  String index(){
+        return  "index";
+    }
+
+    @RequestMapping("/about")
+    @ResponseBody
+    public  String about(){
+        return "About Page";
+    }
+}
